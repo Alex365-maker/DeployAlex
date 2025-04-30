e694408e7804295c1f931b14b9108b8049d5f3f8
Alex365-maker <70705449+Alex365-maker@users.noreply.github.com>
2025-04-30 13:04:41 +0300

    am

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const Home = lazy(() => import('./components/Home'));
